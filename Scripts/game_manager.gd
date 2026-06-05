644d5557eded972a525aa21822adcc95ac273f62
Eysteinn Örn Jónsson <eysi@live.com>
2026-06-05 22:42:24 +0000

    fixed crashing on game start

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,10 +1,3 @@
 extends Node
 
 @onready var eclipse_timer: Timer = %EclipseTimer
-
-func _ready():
-    eclipse_timer.timeout.connect(_on_eclipse_timer_timeout)
-
-
-func _on_eclipse_timer_timeout(): #Game over when the timer runs out, but you can change this to do something else if you want.
-    pass
