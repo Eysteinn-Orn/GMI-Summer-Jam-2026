6661fffd839367668d8204a9e372f083b33b78fe
Herbie <herbie.james@protonmail.com>
2026-06-09 15:39:23 +0100

    add camera easing

--- Scripts/camera_zones.gd
+++ Scripts/camera_zones.gd
@@ -1,11 +1,16 @@
 extends Camera2D
 
 ## assumes value of zoom = Vector2(4.0, 4.0)
+@export var camera_grid : TextureRect
 @export var player : Node
 const camera_size : Vector2 = Vector2(480, 270)
 const hud_height  : float   = 202
 const bounds_size : Vector2 = Vector2(camera_size.x, camera_size.y - hud_height/4)
 const offset_y    : float   = - hud_height / 8
+const SPEED = 1000.0
+
+func _ready() -> void:
+	camera_grid.visible = false
 
 func get_camera_pos(focus: Vector2) -> Vector2:
 	var camera_index : Vector2 = Vector2(
@@ -17,5 +22,8 @@ func get_camera_pos(focus: Vector2) -> Vector2:
 		camera_index.y * bounds_size.y
 	)
 
-func _process(_delta: float) -> void:
-	position = get_camera_pos(player.position)
+func _process(delta: float) -> void:
+	position = Vector2(
+		move_toward(position.x, get_camera_pos(player.position).x, delta * SPEED),
+		move_toward(position.y, get_camera_pos(player.position).y, delta * SPEED)
+	)
